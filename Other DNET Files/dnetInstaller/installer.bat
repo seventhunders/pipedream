9c642fdf6c603d4cdfb71e61f44e359d94ce4e1b
unknown <Chris@.(none)>
2009-09-14 23:53:23 -0500

    installer fixes

--- Other DNET Files/dnetInstaller/installer.bat
+++ Other DNET Files/dnetInstaller/installer.bat
@@ -1,13 +1,11 @@
 Git-1.6.4-preview20090730.exe
 python-2.6.2.msi
+rmdir /S c:\dnet
 mkdir C:\dnet
-xcopy "zebedee-2.4.1A" "C:\dnet\zebedee-2.4.1A\"
+xcopy checkout.bat c:\dnet\
 cd C:\dnet\
-path = %PATH%;"C:\Program Files\Git\bin\"
-git clone git://github.com/seventhunders/pipedream.git
-cd pipedream\
+cmd.exe /c c:\dnet\checkout.bat
+cd pipedream
 git checkout -b windows origin/windows
-cd ../
-xcopy "zebedee-2.4.1A" "C:\dnet\pipedream\zebedee-2.4.1A\"
-rm zebedee-2.4.1A -rf
+superinstall.py
 pause
